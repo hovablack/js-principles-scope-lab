90ddf32062aac666dfe424632bd6364b0f1747ab
Hova Black <nevermind92@hotmail.com>
2021-07-29 18:42:56 -0500

    Test passing

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'other bob';
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBest) {
+  bestCustomer = newBest;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'same bob';
+}
